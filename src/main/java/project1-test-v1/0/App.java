a844b7b4f21b0aaeda059bb332875a565ff84ddd
venkatesh <vnagavenkatesh502@gmail.com>
2021-10-13 22:18:46 +0530

    priject01-test_code

---
+++ src/main/java/project1-test-v1/0/App.java
@@ -0,0 +1,13 @@
+package project1-test-v1.0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello venkat love you!" );
+    }
+}
